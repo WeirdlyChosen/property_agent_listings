b7762dfb8112e728a6b1517e560fe4300f86aab1
WeirdlyChosen <sampahtj@gmail.com>
2025-09-23 10:07:06 +0700

    bugfix property listing update kecamatan to show kecamatan_name as title

--- property_agent_listings/property_agent_listings/doctype/property_listing/property_listing.py
+++ property_agent_listings/property_agent_listings/doctype/property_listing/property_listing.py
@@ -1,73 +1,73 @@
-import frappe
 import uuid
-from frappe.website.website_generator import WebsiteGenerator
+
+import frappe
 from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
 class PropertyListing(WebsiteGenerator):
-    def autoname(self):
-        """
-        Override autoname to generate 8-char UID for 'kode' field if it's empty.
-        Also set it as doc.name if you want.
-        """
-        if not self.kode:
-            self.kode = uuid.uuid4().hex[:8]
+	def autoname(self):
+		"""
+		Override autoname to generate 8-char UID for 'kode' field if it's empty.
+		Also set it as doc.name if you want.
+		"""
+		if not self.kode:
+			self.kode = uuid.uuid4().hex[:8]
 
-        # If you also want to use it as the document name
-        self.name = self.kode
-        
-    def validate(self):
-        """Recalculate harga per meter persegi before saving"""
-        if self.harga and self.luas_tanah:
-            try:
-                self.permeter = self.harga / self.luas_tanah
-            except ZeroDivisionError:
-                self.permeter = 0
-        else:
-            # If either field is missing, reset to 0
-            self.permeter = 0
+		# If you also want to use it as the document name
+		self.name = self.kode
 
-        # --- HTML field updates ---
-        # Example: turn google_drive_folder into clickable link
-        if self.google_drive_folder:
-            self.google_drive_link = f'<a href="{self.google_drive_folder}" target="_blank">Open Google Drive</a>'
-        else:
-            self.google_drive_link = ""
+	def validate(self):
+		"""Recalculate harga per meter persegi before saving"""
+		if self.harga and self.luas_tanah:
+			try:
+				self.permeter = self.harga / self.luas_tanah
+			except ZeroDivisionError:
+				self.permeter = 0
+		else:
+			# If either field is missing, reset to 0
+			self.permeter = 0
 
-        # --- Alamat Property ---
-        parts = []
+		# --- HTML field updates ---
+		# Example: turn google_drive_folder into clickable link
+		if self.google_drive_folder:
+			self.google_drive_link = (
+				f'<a href="{self.google_drive_folder}" target="_blank">Open Google Drive</a>'
+			)
+		else:
+			self.google_drive_link = ""
 
-        if self.nama_jalan:
-            parts.append(f"{self.nama_jalan} ")
+		# --- Alamat Property ---
+		parts = []
 
-        if self.perumahan:
-            perumahan_name = frappe.db.get_value("Perumahan", self.perumahan, "perumahan_name")
-            if perumahan_name:
-                parts.append(perumahan_name)
+		if self.nama_jalan:
+			parts.append(f"{self.nama_jalan} ")
 
-        if self.perumahan_cluster:
-            cluster_name = frappe.db.get_value("Perumahan Cluster", self.perumahan_cluster, "perumahan_cluster_name")
-            if cluster_name:
-                parts.append(f" Cluster {cluster_name}")
+		if self.perumahan:
+			perumahan_name = frappe.db.get_value("Perumahan", self.perumahan, "perumahan_name")
+			if perumahan_name:
+				parts.append(perumahan_name)
 
-        if self.blok_perumahan:
-            parts.append(f" Blok {self.blok_perumahan}")
+		if self.perumahan_cluster:
+			cluster_name = frappe.db.get_value("Perumahan Cluster", self.perumahan_cluster, "cluster_name")
+			if cluster_name:
+				parts.append(f" Cluster {cluster_name}")
 
-        if self.nomor_rumah:
-            # If blok_perumahan is empty, add space before nomor
-            sep = " " if not self.blok_perumahan else "-"
-            parts.append(f"{sep}{self.nomor_rumah}")
+		if self.blok_perumahan:
+			parts.append(f" Blok {self.blok_perumahan}")
 
-        kecamatan_name = getattr(self, "kecamatan_name", None)
-        if self.kecamatan_name:
-            parts.append(f" {self.kecamatan_name}")
+		if self.nomor_rumah:
+			# If blok_perumahan is empty, add space before nomor
+			sep = " " if not self.blok_perumahan else "-"
+			parts.append(f"{sep}{self.nomor_rumah}")
 
-        if self.kota:
-            parts.append(f" {self.kota}")
+		if self.kecamatan:
+			parts.append(f" {self.kecamatan}")
 
-        if self.provinsi:
-            parts.append(f" {self.provinsi}")
+		if self.kota:
+			parts.append(f" {self.kota}")
 
-        self.alamat_property = "".join(parts).strip()
+		if self.provinsi:
+			parts.append(f" {self.provinsi}")
 
-            
+		self.alamat_property = "".join(parts).strip()
